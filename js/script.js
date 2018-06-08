e0b9b7dc07999dc2ba8db7136d3f0cdbaaccb918
romanzakyan <39835048+romanzakyan@users.noreply.github.com>
2018-06-08 15:46:32 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -245,11 +245,11 @@ $( window ).resize(function() {
 });
 
 if($( window ).width() < 1260 && $( window ).width() > 960){
-	$('.div_1').css('height', '350px');
+	$('.div_1').css('height', '400px');
 }else if($( window ).width() <= 960 && $( window ).width() > 450){
-	$('.div_1').css('height', '350px');
+	$('.div_1').css('height', '400px');
 }else{
-	$('.div_1').css('height', '350px');
+	$('.div_1').css('height', '400px');
 }
 
 $('.div_2').css('height', $('.div_1').css('height'));
